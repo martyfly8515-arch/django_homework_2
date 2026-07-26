843eea0db62e0a57f26b9815fd8047ab0f79dff5
martyfly8515-arch <martyfly8515@gmail.com>
2026-07-26 20:24:59 +0500

    Delete validators.py

--- validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_non_negative(value):
-    if value < 0:
-        raise ValidationError(
-            'Значение должно быть положительным числом или равно нулю.'
-        )
